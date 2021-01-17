08e2af0f2c5a32db5f958bf5b67e1cbd6e265c2d
RAFH82 <hrechkar@gmail.com>
2021-01-16 16:43:39 -0800

    - Added Materiul UI popup and form fill components - Added pop-up when clicking on the "Open New Bar" button.  - Added form field within that pop-up  - Modified addBar func to set state of input to blank when submitting bar name  - Disabled button ripple animation when clicked on, by passing props to the material ui button component (disableFocusRipple & disableRipple)

--- src/Street.js
+++ src/Street.js
@@ -8,9 +8,17 @@ import BarListing from "./BarListing";
 import "./Street.css";
 import "./BarListing.css";
 
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
 function Street() {
 	const [channels, setChannels] = useState([]);
 	const [{ user, userLocation }] = useStateValue();
+	const [anchorEl, setAnchorEl] = useState(null);
+	const [input, setInput] = useState("");
 
 	useEffect(() => {
 		db.collection("bars").onSnapshot((snapshot) => {
@@ -27,7 +35,7 @@ function Street() {
 		});
 	}, []);
 
-	// flag for rendering info telling user no bars are near them
+	// flag for rendering information telling user no bars are near them
 	let atLeastOneBar;
 
 	const closeProximityToUser = (location) => {
@@ -52,27 +60,99 @@ function Street() {
 		return closeProximity;
 	};
 
-	const addBar = () => {
-		const barName = prompt("Please enter a Bar name");
-
-		if (barName) {
+	const addBar = (event) => {
+		event.preventDefault();
+		if (input) {
 			db.collection("bars").add({
 				table_size: 6,
 				capacity: 60,
-				name: barName,
+				name: input,
 				location: new firebase.firestore.GeoPoint(
 					userLocation.latitude,
 					userLocation.longitude
 				),
 			});
 		}
+		setInput("");
+		handleClose();
 	};
+
+	// start of stuff for bar name popup input
+	const useStyles = makeStyles((theme) => ({
+		root: {
+			"& > *": {
+				margin: theme.spacing(1),
+				width: "25ch",
+			},
+		},
+		typography: {
+			padding: theme.spacing(2),
+		},
+	}));
+
+	const classes = useStyles();
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const open = Boolean(anchorEl);
+	const id = open ? "simple-popover" : undefined;
+	// end of stuff for bar name popup input
 
 	return (
 		<div className="street">
 			<div className="street_header">
 				<div>Open Bars</div>
-				<div onClick={addBar}>Open New Bar</div>
+				<div>
+					<Button
+						id="form_close"
+						variant="contained"
+						color="primary"
+						onClick={handleClick}
+						disableFocusRipple={true}
+						disableRipple={true}
+					>
+						Open New Bar
+					</Button>
+					<Popover
+						id={id}
+						open={open}
+						anchorEl={anchorEl}
+						onClose={handleClose}
+						anchorOrigin={{
+							vertical: "bottom",
+							horizontal: "center",
+						}}
+						transformOrigin={{
+							vertical: "top",
+							horizontal: "center",
+						}}
+					>
+						<Typography className={classes.typography}>
+							<form className={classes.root} noValidate autoComplete="off">
+								<TextField
+									id="outlined-basic"
+									label="Enter your bar's name"
+									variant="outlined"
+									onChange={(event) => setInput(event.target.value)}
+									value={input}
+								/>
+								<div>
+									<button
+										id="input__bar_box"
+										type="submit"
+										onClick={addBar}
+									></button>
+								</div>
+							</form>
+						</Typography>
+					</Popover>
+				</div>
 			</div>
 			<div className="bar_list">
 				{channels.map((channel) =>
