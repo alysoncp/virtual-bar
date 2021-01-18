d9feecc7157ad70577e19e9df2138d9985ea2583
RAFH82 <hrechkar@gmail.com>
2021-01-17 17:05:29 -0800

    - Added userCanDelete function for tables - Added delete and join table buttons

--- src/Table.js
+++ src/Table.js
@@ -1,23 +1,41 @@
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
-
+import db from "./firebase";
+import { useStateValue } from "./hooks+context/StateProvider";
 import "./Table.css";
 
-function Table({ id, name }) {
+function Table({ id, name, tableCreatorId }) {
 	const { barId } = useParams();
 	const history = useHistory();
+	const [{ idToken }] = useStateValue();
 
 	const goToTable = () => {
 		history.push(`/bar/${barId}/table/${id}`);
 	};
 
+	const userCanDelete = (tableIdToken, userIdToken) => {
+		return tableIdToken === userIdToken ? true : false;
+	};
+
+	const deleteTable = () => {
+		if (window.confirm("Are you sure you want to delete your table?")) {
+			db.collection("bars").doc(barId).collection("tables").doc(id).delete();
+		} else {
+			return;
+		}
+	};
+
 	return (
-		<div className="table" onClick={goToTable}>
+		<div className="table">
 			<img
 				src="https://www.flaticon.com/svg/static/icons/svg/160/160705.svg"
 				alt="table logo"
 			/>
 			<div className="title">{`The #${name} table!!`}</div>
+			<button onClick={goToTable}>Join Table</button>
+			{userCanDelete(tableCreatorId, idToken) && (
+				<button onClick={deleteTable}>Delete Table</button>
+			)}
 		</div>
 	);
 }
