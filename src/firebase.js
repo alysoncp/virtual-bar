18405dd9f03e25c0b397bdb4fd568436ca3179bf
alysoncp <alyson.c.pickett@gmail.com>
2021-01-18 10:47:57 -0800

    fixed typo

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
-import React from "react";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASEAPIKEY,
