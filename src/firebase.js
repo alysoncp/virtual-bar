7c8e6273551f39418bcc5d6650e0d744efb8e794
alysoncp <alyson.c.pickett@gmail.com>
2021-01-17 20:56:59 -0800

    Moved firebase API key to private .env variable now that we have billing enabled on Firebase...

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 
-// const firebaseConfig = {
-// 	apiKey: "AIzaSyDpmjbTApTaIDEUAIs-I9Gtafhx0n8ias4",
-// 	authDomain: "slack-clone-13896.firebaseapp.com",
-// 	projectId: "slack-clone-13896",
-// 	storageBucket: "slack-clone-13896.appspot.com",
-// 	messagingSenderId: "1029680900759",
-// 	appId: "1:1029680900759:web:4181a55e42e1e3b452d7d2",
-// 	measurementId: "G-DFKXED0DXX",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAZtTYp-SCK5E7bZ2jZtGtH_ZqRAWIn7AU",
+  apiKey: REACT_APP_FIREBASEAPIKEY,
   authDomain: "virtual-bar-9a045.firebaseapp.com",
   projectId: "virtual-bar-9a045",
   storageBucket: "virtual-bar-9a045.appspot.com",
