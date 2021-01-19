ed2a110cc344171c2a1f8c3851129ffe26a35119
RAFH82 <hrechkar@gmail.com>
2021-01-19 10:32:08 -0800

    User can now click on Bar/Table image to enter

--- src/BarListing.js
+++ src/BarListing.js
@@ -1,30 +1,30 @@
-// React 
+// React
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { useStateValue } from "./hooks+context/StateProvider";
 import db from "./firebase";
 
 // Material UI
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 // Custom CSS
 import "./BarListing.css";
 
 // Material UI Styles
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
+	root: {
+		maxWidth: 345,
+	},
+	media: {
+		height: 140,
+	},
 });
 
 function BarListing({ Icon, title, id, addChannelOption, barCreatorId }) {
@@ -57,34 +57,35 @@ function BarListing({ Icon, title, id, addChannelOption, barCreatorId }) {
 	};
 
 	return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://images.unsplash.com/photo-1575444758702-4a6b9222336e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YmFyfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-					{title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Put description here.........
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" onClick={selectChannel}>
-          Join Bar
-        </Button>
+		<Card className={classes.root}>
+			<CardActionArea>
+				<CardMedia
+					onClick={selectChannel}
+					className={classes.media}
+					image="https://images.unsplash.com/photo-1575444758702-4a6b9222336e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YmFyfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
+					title="Contemplative Reptile"
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="h2">
+						{title}
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">
+						Put description here.........
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Button size="small" color="primary" onClick={selectChannel}>
+					Join Bar
+				</Button>
 				{userCanDelete(barCreatorId, idToken) && (
 					<Button size="small" color="primary" onClick={deleteBar}>
 						Delete Bar
 					</Button>
 				)}
-      </CardActions>
-    </Card>
-  );
+			</CardActions>
+		</Card>
+	);
 
 	// return (
 	// 	<div className="bar_listing">
