cda19b08ef0bcb995c81520cc5c08c16ef68faf2
alysoncp <alyson.c.pickett@gmail.com>
2021-01-24 17:16:15 -0800

    setting user state to include bar and table name, pulling in friends list to see where friends are sitting

--- src/hooks+context/useBarTable.js
+++ src/hooks+context/useBarTable.js
@@ -1,22 +1,46 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import db from "../firebase";
 import { useStateValue } from "./StateProvider";
 
 export default function useBarTable() {
 
   const [{ user, at_bar, at_table, last_bar, last_table }] = useStateValue();
+  
+  let at_bar_name;
+  let at_table_name;
 
-  useEffect(() => {
-		console.log("At bar: ", at_bar);
-    console.log("At table: ", at_table);
-    console.log("Last bar: ", last_bar);
-    console.log("Last table: ", last_table);
+  useEffect(async () => {
     
     if(user) {
-      db.collection("users")
+
+      if(at_bar) {
+        const snap = await db.collection("bars")
+          .doc(at_bar)
+          .get()
+        at_bar_name = snap.data().name
+      } else {
+        at_bar_name = null;
+      }
+
+      if(at_table) {
+        const snap = await db.collection("bars")
+          .doc(at_bar)
+          .collection("tables")
+          .doc(at_table)
+          .get()
+        at_table_name = snap.data().name
+      } else {
+        at_table_name = null;
+      }
+
+      console.log("Bar named: ", at_bar_name);
+      console.log("Table named: ", at_table_name);
+
+       db.collection("users")
         .doc(user.uid)
         .update({
           at_bar: at_bar,
+          at_bar_name: at_bar_name,
           at_table: at_table,
           last_bar: last_bar,
           last_table: last_table,
@@ -32,6 +56,12 @@ export default function useBarTable() {
 		  .delete()
       }  
     }
+
+
+    console.log("At bar: ", at_bar);
+    console.log("At table: ", at_table);
+    console.log("Last bar: ", last_bar);
+    console.log("Last table: ", last_table);
            
   }, [at_bar, at_table])
   
